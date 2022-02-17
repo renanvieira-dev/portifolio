2685c884301dd77a8223d64d2c9924f155711ccf
Renan Vieira Nascimento <reenaanfc@gmail.com>
2022-02-17 12:39:37 -0400

    Portifolio

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+function typeWrite(e){
+    const textoArray = e.innerHTML.split('');
+    e.innerHTML = '';
+    textoArray.forEach(function (letter, i){
+        setTimeout(function (){
+            e.innerHTML += letter;
+        }, 80 * i);
+    });
+}
+
+const phrase = document.querySelector('#write');
+typeWrite(phrase);
